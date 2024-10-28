fe0045c810ba4eb33e573b5859c31790ce499412
LukasHamstra <lukashamstra@gmail.com>
2024-10-28 17:53:24 +0100

    Opdr 4.7.2

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -15,6 +15,18 @@ import MCPizzeriaSQL
 
 ### ---------  Functie definities  -----------------
 
+def zoekKlant():
+    #haal de ingevoerde_klantnaam op uit het invoerveld en gebruik dit om met SQL de klant in database te vinden
+    gevonden_klanten = MCPizzeriaSQL.zoekKlantInTabel(ingevoerde_klantnaam.get())
+    print(gevonden_klanten) # om te testen
+    invoerveldKlantnaam.delete(0, END) #invoerveld voor naam leeg maken
+    invoerveldKlantNr.delete(0, END) #invoerveld voor klantNr leeg maken
+    for rij in gevonden_klanten: #voor elke rij dat de query oplevert
+        #toon klantnummer, de eerste kolom uit het resultaat in de invoerveld
+        invoerveldKlantNr.insert(END, rij[0]) 
+        #toon klantAchternaam, de tweede kolom uit het resultaat in de invoerveld
+        invoerveldKlantnaam.insert(END, rij[1]) 
+
 
 ### --------- Hoofdprogramma  ---------------
 
@@ -35,8 +47,11 @@ invoerveldKlantnaam.grid(row=1, column=1, sticky="W")
 labelIntro = Label(venster, text="Klantnummer:")
 labelIntro.grid(row=2, column=0, sticky="W")
 
-invoerveldKlantNr = Entry(venster, state="disabled")
+invoerveldKlantNr = Entry(venster)
 invoerveldKlantNr.grid(row=2, column=1, sticky="W")
+
+knopZoekKlantnaam = Button(venster, text="Zoek klant", width=12, command=zoekKlant)
+knopZoekKlantnaam.grid(row=1, column=4)
 
 knopSluit = Button(venster, text="Sluiten", width=12, command=venster.destroy)
 knopSluit.grid(row=17, column=4)
